fbb7e078f3cb75da21b01be3892bea76360ad58b
zurfyx <zurfyx@gmail.com>
2016-03-06 20:15:14 +0100

    prints lists of categories

--- udlchan/posts/views.py
+++ udlchan/posts/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Category, Post
 
 
 class CategoriesList(TemplateView):
+    """
+    Displays list of categories
+    """
     template_name = "posts/categories.html"
+
+    def get(self, request, *args, **kwargs):
+        self.categories = Category.objects.all()
+        return super(CategoriesList, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoriesList, self).get_context_data(**kwargs)
+        context['categories'] = self.categories
+        return context
