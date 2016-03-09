07f88b9c38d435d52ea767bba9c79b3f32dd7956
zurfyx <zurfyx@gmail.com>
2016-03-09 01:14:08 +0100

    categories add to CreateView

--- udlchan/posts/views.py
+++ udlchan/posts/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView, CreateView, FormView
 from .models import Category, Post
 from .forms import CategoryForm
 
@@ -19,37 +20,13 @@ class CategoriesList(TemplateView):
         return context
 
 
-class CategoriesAdd(TemplateView):
+class CategoriesAdd(CreateView):
     """
     Add category. Provides a form and handling to add a new category.
     """
+    model = Category
     form_class = CategoryForm
-    template_name = "posts/categories_add.html"
+    template_name = 'posts/categories_add.html'
 
-    def get(self, request, *args, **kwargs):
-        return super(CategoriesAdd, self).get(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super(CategoriesAdd, self).get_context_data(**kwargs)
-        if 'form_add' not in context:
-            context['form_add'] = self.form_class()
-        return context
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            self.form_valid(form, **kwargs)
-        else:
-            kwargs = self.form_invalid(form, **kwargs)
-
-        return super(CategoriesAdd, self).get(request, *args, **kwargs)
-
-    def form_valid(self, form, **kwargs):
-        # TODO save form
-        # TODO redirect to list of categories
-        pass
-
-    def form_invalid(self, form, **kwargs):
-        context = super(CategoriesAdd, self).get_context_data(**kwargs)
-        context['form_add'] = form
-        return context
+    def get_success_url(self):
+        return reverse('posts:categories')
