4a272f38f2737a1b089693ddb8968d5808affaba
zurfyx <zurfyx@gmail.com>
2016-03-22 01:53:21 +0100

    ajax comments reply log

--- udlchan/static/js/posts/comment.js
+++ udlchan/static/js/posts/comment.js
@@ -1,23 +1,40 @@
 (function($) {
     /** Submit comment form, then adds it into the HTML. **/
 
+    var COMMENT_ELEMENT = '.element';
     var ELEMENT = 'form.new-comment';
+    var REPLY_FORM = '.reply-form';
+    var REPLY_LOG =  '.reply-log';
 
-    function processAJAXData(data) {
-        if (data.status === 'error') {
+    function processAJAXData(ajaxData, form) {
+        if (ajaxData.status === 'error') {
             alert(data.message);
             return;
         }
-        console.log('Successfully posted! :-)');
+
+        // log
+        var reply_log = '<span>Replied: ' + form.find('[name="content"]').val() + '</span>';
+        form.closest(COMMENT_ELEMENT).find(REPLY_LOG).append(reply_log);
+
+        // clear form
+        console.log(form.find('[name="parent"]') )
+        if (form.find('[name="parent"]').val() == '') {
+            form.find('[name="content"]').val('');
+        } else {
+            form.closest(REPLY_FORM).html('');
+        }
     }
 
     function listener() {
         $('body').on('submit', ELEMENT, function(e) {
-            var href = $(this).attr('action');
-            var data = $(this).serialize();
+            var form = $(this);
+            var href = form.attr('action');
+            var data = form.serialize();
             e.preventDefault();
 
-            $.post(href, data, processAJAXData).fail(function() {
+            $.post(href, data, function(ajaxData) {
+                processAJAXData(ajaxData, form);
+            }).fail(function() {
                 alert('Failed to retrieve data from the server.');
             });
         });
